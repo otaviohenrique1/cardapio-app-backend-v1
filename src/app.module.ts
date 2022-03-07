b4e79ebcde77c1748275684c1f0be7d8b1c31e2e
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-03-07 14:43:53 -0300

    adicionando modulos usuario e refeicao

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsuarioModule } from './usuario/usuario.module';
+import { RefeicaoModule } from './refeicao/refeicao.module';
 
 @Module({
-  imports: [],
+  imports: [UsuarioModule, RefeicaoModule],
   controllers: [AppController],
   providers: [AppService],
 })
